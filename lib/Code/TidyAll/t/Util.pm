dbae4285a4cafdb312cc6829bccbe66e759feb86
Jonathan Swartz <swartz@pobox.com>
2012-09-27 03:21:30 -0700

    add util test

---
+++ lib/Code/TidyAll/t/Util.pm
@@ -0,0 +1,18 @@
+package Code::TidyAll::t::Util;
+use Code::TidyAll::Util qw(dirname tempdir_simple);
+use IPC::System::Simple qw(capturex);
+use Test::Class::Most parent => 'Code::TidyAll::Test::Class';
+
+sub test_tempdir_simple : Tests {
+    return "author testing only" unless ( $ENV{AUTHOR_TESTING} );
+
+    my $dir = capturex(
+        "$^X", "-I",
+        "lib", "-MCode::TidyAll::Util",
+        "-e",  "print Code::TidyAll::Util::tempdir_simple "
+    );
+    ok( -d dirname($dir), "parent exists" );
+    ok( !-d $dir,         "dir does not exist" );
+}
+
+1;
---
+++ t/Util.t
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+use Code::TidyAll::t::Util;
+Code::TidyAll::t::Util->runtests;
