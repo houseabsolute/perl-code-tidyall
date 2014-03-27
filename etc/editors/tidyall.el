315f86a73fd6c2022244ac469517b9e72530f1b3
George Hartzell <georgewh@gene.com>
2014-03-27 13:33:08 -0700

    emacs mode: clean up temp error-file.
    The emacs lisp code creates a temporary file to hold any errors
    that tidyall spews.  We should clean it up when we're finished
    with it.
    
    My emacs-fu isn't strong enough to deal with cleaning up the file
    if something exceptional happens (e.g. what's the elisp equiv. of
    a "finally" clause in try/catch/finally?)

--- etc/editors/tidyall.el
+++ etc/editors/tidyall.el
@@ -106,4 +106,5 @@
                         (insert-file-contents error-file))
                       (when (< (length (window-list)) 2)
                         (split-window-vertically))
-                      (set-window-buffer (next-window) error-buffer)))))))))
+                      (set-window-buffer (next-window) error-buffer))))
+             (delete-file error-file))))))
