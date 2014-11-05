c3185c0d96ef017ce84337685165b680edb181f0
Dave Rolsky <autarch@urth.org>
2014-11-05 10:56:20 -0600

    Pass -npro for all PerlTidy plugin tests to make sure we don't pick up an existing profile

--- t/lib/Test/Code/TidyAll/Plugin/PerlTidy.pm
+++ t/lib/Test/Code/TidyAll/Plugin/PerlTidy.pm
@@ -7,24 +7,27 @@ sub test_main : Tests {
 
     my $source = 'if (  $foo) {\nmy   $bar =  $baz;\n}\n';
     $self->tidyall(
+        conf        => { argv => '-npro' },
         source      => $source,
         expect_tidy => 'if ($foo) {\n    my $bar = $baz;\n}\n'
     );
     $self->tidyall(
-        conf        => { argv => '-bl' },
-        source      => $source,
+        conf   => { argv => '-npro -bl' },
+        source => $source,
         expect_tidy => 'if ($foo)\n{\n    my $bar = $baz;\n}\n'
     );
     $self->tidyall(
+        conf      => { argv => '-npro' },
         source    => 'if ($foo) {\n    my $bar = $baz;\n}\n',
         expect_ok => 1
     );
     $self->tidyall(
+        conf         => { argv => '-npro' },
         source       => 'if ($foo) {\n    my $bar = $baz;\n',
         expect_error => qr/Final nesting depth/
     );
     $self->tidyall(
-        conf         => { argv => '--badoption' },
+        conf         => { argv => '-npro --badoption' },
         source       => $source,
         expect_error => qr/Unknown option: badoption/
     );
