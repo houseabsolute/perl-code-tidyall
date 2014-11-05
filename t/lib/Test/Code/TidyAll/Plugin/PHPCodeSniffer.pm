4e1708e05d2dd2a897714367a86f4fd6378cc294
Dave Rolsky <autarch@urth.org>
2014-11-05 11:34:49 -0600

    Add PHP Code Sniffer to repo for benefit of tests

--- t/lib/Test/Code/TidyAll/Plugin/PHPCodeSniffer.pm
+++ t/lib/Test/Code/TidyAll/Plugin/PHPCodeSniffer.pm
@@ -4,30 +4,30 @@ use Test::Class::Most parent => 'Test::Code::TidyAll::Plugin';
 
 sub test_filename { 'foo.php' }
 
+sub _extra_path {
+    'php5/usr/bin';
+}
+
 sub test_main : Tests {
     my $self   = shift;
 
-    local $ENV{PATH} = $ENV{PATH};
-    $ENV{PATH} .= ':/usr/local/pear/bin'
-        unless $ENV{PATH} =~ q{/usr/local/pear/bin};
-
-    $self->require_executable('phpcs');
+    $self->require_executable('php');
 
     my $source = '<?php function foo() { $bar = 5 } ?>';
 
     $self->tidyall(
         source    => $source,
-        conf      => { cmd => $cmd, argv => "--severity=6" },
+        conf      => { argv => "--severity=6" },
         expect_ok => 1,
     );
     $self->tidyall(
         source       => $source,
-        conf         => { cmd => $cmd, argv => "--severity=3" },
+        conf         => { argv => "--severity=3" },
         expect_error => qr/Missing .* doc/,
     );
     $self->tidyall(
         source       => $source,
-        conf         => { cmd => $cmd, argv => "--blahblah" },
+        conf         => { argv => "--blahblah" },
         expect_error => qr/not known/,
     );
 }
---
+++ xt/author/Plugin-PHPCodeSniffer.t
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+use lib 't/lib';
+use Test::Code::TidyAll::Plugin::PHPCodeSniffer;
+Test::Code::TidyAll::Plugin::PHPCodeSniffer->runtests;
