f03d1f82a3230b331b7acae14b65eaea919d2304
Dave Rolsky <autarch@urth.org>
2014-11-06 15:35:14 -0600

    Use IPC::Run3 instead of Capture::Tiny in some cases
    The latter seems to sometimes return output marked as UTF-8 even when we try
    to set the STDOUT and STDERR layers to bytes. IPC::Run3 is much more
    predictable (always bytes).
    
    This is a band-aid. This whole distro needs a thorough review of its charset
    handling, and ideally would provide a way to specify the encoding of each file
    that's tidied.

--- t/lib/Test/Code/TidyAll/Plugin/JSBeautify.pm
+++ t/lib/Test/Code/TidyAll/Plugin/JSBeautify.pm
@@ -1,6 +1,9 @@
 package Test::Code::TidyAll::Plugin::JSBeautify;
 
+use Encode qw(encode);
+use File::Slurp::Tiny qw(write_file);
 use Test::Class::Most parent => 'Test::Code::TidyAll::Plugin';
+use Test::Warnings qw(warnings);
 
 sub _extra_path {
     'node_modules/.bin';
@@ -20,6 +23,46 @@ sub test_main : Tests {
         source      => $source,
         conf        => { argv => '--indent-size 3 --brace-style expand' },
         expect_tidy => 'sp.toggleResult = function(id)\n{\n   foo(id)\n}',
+    );
+}
+
+sub test_utf8 : Tests {
+    my $self = shift;
+
+    my $contents
+        = encode( 'UTF-8', qq{var unicode  =  "Unicode - \x{263a}";} );
+    my $expect = encode( 'UTF-8', qq{var unicode = "Unicode - \x{263a}";} );
+
+    local $SIG{__WARN__} = sub { Carp::cluck(@_) };
+    is_deeply(
+        [
+            warnings {
+                $self->tidyall(
+                    source      => $contents,
+                    expect_tidy => $expect,
+                    desc        => 'tidy UTF-8 from string',
+                );
+            }
+        ],
+        [],
+        'no warnings tidying UTF-8 source'
+    );
+
+    my $file = $self->{root_dir} . '/test.js';
+    write_file( $file, $contents );
+
+    is_deeply(
+        [
+            warnings {
+                $self->tidyall(
+                    source_file => $file,
+                    expect_tidy => $expect,
+                    desc        => 'tidy UTF-8 from file',
+                );
+            }
+        ],
+        [],
+        'no warnings tidying UTF-8 source'
     );
 }
 
