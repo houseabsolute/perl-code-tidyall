7d970d5c888e3c758de6779c1ecc2158dc92afc9
Dave Rolsky <autarch@urth.org>
2015-03-16 11:38:33 -0500

    Remove use of Moose::Autobox in custom Pod::Weaver plugin

--- inc/Pod/Weaver/Section/SeeAlsoCodeTidyAll.pm
+++ inc/Pod/Weaver/Section/SeeAlsoCodeTidyAll.pm
@@ -5,7 +5,6 @@ use namespace::autoclean;
 use Moose;
 with 'Pod::Weaver::Role::Section';
 
-use Moose::Autobox;
 use Pod::Elemental::Selectors;
 
 # Add "SEE ALSO: Code::TidyAll"
@@ -16,26 +15,26 @@ sub weave_section {
     return if $input->{filename} =~ m{\QCode/TidyAll.pm};
 
     my $idc = $input->{pod_document}->children;
-    for ( my $i = 0 ; $i < $idc->length ; $i++ ) {
+    for ( my $i = 0; $i < scalar @{$idc}; $i++ ) {
         next unless my $para = $idc->[$i];
         return
-             if $para->can('command')
-          && $para->command eq 'head1'
-          && $para->content eq 'SEE ALSO';
+               if $para->can('command')
+            && $para->command eq 'head1'
+            && $para->content eq 'SEE ALSO';
     }
-    $document->children->push(
+
+    push @{ $document->children },
         Pod::Elemental::Element::Nested->new(
-            {
-                command  => 'head1',
-                content  => 'SEE ALSO',
-                children => [
-                    Pod::Elemental::Element::Pod5::Ordinary->new(
-                        { content => "L<Code::TidyAll>" }
-                    ),
-                ],
-            }
-        ),
-    );
+        {
+            command  => 'head1',
+            content  => 'SEE ALSO',
+            children => [
+                Pod::Elemental::Element::Pod5::Ordinary->new(
+                    { content => "L<Code::TidyAll>" }
+                ),
+            ],
+        }
+        );
 }
 
 __PACKAGE__->meta->make_immutable;
