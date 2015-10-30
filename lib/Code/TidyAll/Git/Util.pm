174de186ab1a626ebabacf3e932727cb36832e17
Ricardo Signes <rjbs@cpan.org>
2015-10-30 10:30:27 -0400

    use git status --porcelain -z
    This was surprisingly obnoxious.
    
    It sounds great, at first, that git uses \0 terminators, but its
    records are of variable length, and the record and field separators
    are the same -- \0!
    
    So, first you read in the change mode (in format "XY ") and then the
    filename ("filename\0").  Then, if the mode was a rename, you read in
    a second filename and throw it away.  The first filename is the
    renamed-to filename.  (That's backward from the for-humans "x -> y"
    format.)
    
    Still, this is less ambiguous.  For example, if there are two ->
    arrows in the human-readable "git status," the correct filename to
    pick out is indeterminable.  Is it worth adding this complexity to
    deal with human boneheadedness?  I leave that decision up to you.

--- lib/Code/TidyAll/Git/Util.pm
+++ lib/Code/TidyAll/Git/Util.pm
@@ -1,7 +1,7 @@
 package Code::TidyAll::Git::Util;
 
 use Cwd qw(realpath);
-use Code::TidyAll::Util qw(pushd);
+use Code::TidyAll::Util qw(pushd rel2abs);
 use IPC::System::Simple qw(capturex);
 use List::MoreUtils qw(uniq);
 use strict;
@@ -14,12 +14,44 @@ our @EXPORT_OK = qw(git_uncommitted_files);
 
 sub git_uncommitted_files {
     my ($dir) = @_;
+    my $pushd = pushd( realpath($dir) );
+    return
+        map { rel2abs($_) }
+        _relevant_files_from_status( capturex(qw( git status --porcelain -z )) );
+}
 
-    $dir = realpath($dir);
-    my $pushd  = pushd($dir);
-    my $output = capturex( "git", "status" );
-    my @files  = ( $output =~ /(?:new file|modified):\s+(.*)/g );
-    @files = uniq( map {"$dir/$_"} @files );
+sub _relevant_files_from_status {
+    my ($status) = @_;
+
+    my @files;
+    {
+        local $_ = $status;
+
+        # There can't possibly be more records than nuls plus one, so we use this
+        # as an upper bound on passes.
+        my $times = tr/\0/\0/;
+
+        for my $i ( 0 .. $times ) {
+            last if /\G\Z/gc;
+
+            /\G(..) /g;
+            my $mode = $1;
+
+            /\G([^\0]+)\0/g;
+            my $name = $1;
+
+            # on renames, parse but throw away the "renamed from" filename
+            if ( $mode =~ /R/ ) {
+                /\G([^\0]+)\0/g;
+            }
+
+            # deletions and renames don't cause tidying
+            next unless $mode =~ /[MAC]/;
+
+            push @files, $name;
+        }
+    }
+
     return @files;
 }
 
