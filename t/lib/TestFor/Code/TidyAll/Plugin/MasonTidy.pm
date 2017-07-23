95b857dae0c6bda3263a5253475b0851b74e2f74
Dave Rolsky <autarch@urth.org>
2017-07-23 14:17:43 -0500

    Use double quotes for strings with newlines in MasonTidy test

--- t/lib/TestFor/Code/TidyAll/Plugin/MasonTidy.pm
+++ t/lib/TestFor/Code/TidyAll/Plugin/MasonTidy.pm
@@ -7,26 +7,26 @@ sub test_main : Tests {
 
     my $source;
 
-    $source = '%if($foo) {\n%bar(1,2);\n%}';
+    $source = "%if(\$foo) {\n%bar(1,2);\n%}";
     $self->tidyall(
         source      => $source,
         conf        => { argv => '-m 1' },
-        expect_tidy => '% if ($foo) {\n%     bar( 1, 2 );\n% }'
+        expect_tidy => "% if (\$foo) {\n%     bar( 1, 2 );\n% }",
     );
     $self->tidyall(
         source      => $source,
         conf        => { argv => q{-m 1 --perltidy-argv="-pt=2 -i=3"} },
-        expect_tidy => '% if ($foo) {\n%    bar(1, 2);\n% }'
+        expect_tidy => "% if (\$foo) {\n%    bar(1, 2);\n% }",
     );
     $self->tidyall(
         source      => $source,
         conf        => { argv => q{-m 2 --perltidy-line-argv=" "} },
-        expect_tidy => '% if ($foo) {\n%     bar( 1, 2 );\n% }'
+        expect_tidy => "% if (\$foo) {\n%     bar( 1, 2 );\n% }",
     );
     $self->tidyall(
         source       => $source,
         conf         => { argv => '-m 1 --badoption' },
-        expect_error => qr/Usage/
+        expect_error => qr/Usage/,
     );
 }
 
