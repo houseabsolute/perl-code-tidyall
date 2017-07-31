b8d07f93efe52d7bfd819c4bcecc6c99b6cca955
Dave Rolsky <autarch@urth.org>
2017-07-31 11:31:54 -0400

    Quote rc file path in Perl Critic tests
    On Windows this path is quite likely to contain spaces.

--- t/lib/TestFor/Code/TidyAll/Plugin/PerlCritic.pm
+++ t/lib/TestFor/Code/TidyAll/Plugin/PerlCritic.pm
@@ -9,30 +9,30 @@ sub test_main : Tests {
     $rc_file->spew("only = 1\nseverity = 1\n[TestingAndDebugging::RequireUseStrict]\n");
 
     $self->tidyall(
-        source       => 'my $foo = 5\n',
-        conf         => { argv => "--profile $rc_file" },
+        source       => "my \$foo = 5\n",
+        conf         => { argv => qq{--profile "$rc_file"} },
         expect_error => qr/Code before strictures/,
     );
     $self->tidyall(
-        source    => 'use strict;\nuse warnings;\nmy $foo = 5\n',
-        conf      => { argv => "--profile $rc_file" },
+        source    => "use strict;\nuse warnings;\nmy \$foo = 5\n",
+        conf      => { argv => qq{--profile "$rc_file"} },
         expect_ok => 1,
     );
     $rc_file->spew("only = 1\nseverity = 1\n[CodeLayout::ProhibitHardTabs]\n");
     $self->tidyall(
-        source    => 'my $foo = 5\n',
-        conf      => { argv => "--profile $rc_file" },
+        source    => "my \$foo = 5\n",
+        conf      => { argv => qq{--profile "$rc_file"} },
         expect_ok => 1,
     );
     $self->tidyall(
-        source       => 'my $foo = 5\n',
-        conf         => { argv => "--profile $rc_file --badoption" },
+        source       => "my \$foo = 5\n",
+        conf         => { argv => qq{--profile "$rc_file" --badoption} },
         expect_error => qr/Unknown option: badoption/
     );
     $rc_file->spew("badconfig = 1\n");
     $self->tidyall(
-        source       => 'my $foo = 5\n',
-        conf         => { argv => "--profile $rc_file" },
+        source       => "my \$foo = 5\n",
+        conf         => { argv => qq{--profile "$rc_file"} },
         expect_error => qr/"badconfig" is not a supported option/
     );
 }
