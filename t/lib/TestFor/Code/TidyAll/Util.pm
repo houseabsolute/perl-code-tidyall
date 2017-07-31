cb9cb8d8c978720de6c866e311eaecf394f79719
Dave Rolsky <autarch@urth.org>
2017-07-31 10:56:48 -0400

    Simplify Util test
    There's no need to fork a new process just to test that a tempdir is deleted
    when the object goes out of scope.

--- t/lib/TestFor/Code/TidyAll/Util.pm
+++ t/lib/TestFor/Code/TidyAll/Util.pm
@@ -1,19 +1,18 @@
 package TestFor::Code::TidyAll::Util;
 
-use IPC::System::Simple qw(capturex);
-use Path::Tiny qw(path);
+use Code::TidyAll::Util qw(tempdir_simple);
+use Path::Tiny;
 use Test::Class::Most parent => 'TestHelper::Test::Class';
 
 sub test_tempdir_simple : Tests {
-    my $dir = path(
-        capturex(
-            $^X,   '-I',
-            'lib', '-MCode::TidyAll::Util',
-            '-e',  'print Code::TidyAll::Util::tempdir_simple'
-        )
-    );
+    my $dir;
+    {
+        my $tempdir = tempdir_simple();
+        $dir = path($tempdir);
+        ok( -d $tempdir, 'tempdir exists' );
+    }
     ok( -d $dir->parent, 'parent exists' );
-    ok( !-d $dir,        'dir does not exist' );
+    ok( !-d $dir,        'tempdir does not exist after it goes out of scope' );
 }
 
 1;
