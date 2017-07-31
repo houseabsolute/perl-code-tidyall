b682a473ad411e1cbe351000f8b624225cf16b0a
Dave Rolsky <autarch@urth.org>
2017-07-31 10:55:42 -0400

    On Win32 get the long path name for temp dirs
    This fixes a number of tests on Windows. Other FS operations were getting
    paths with long path names as opposed to short names like "FOOBAR~1". When
    Code::TidyAll looked compared the two, it thought that files with long names
    were not located under the root dir, which had a short name.

--- lib/Code/TidyAll/Util.pm
+++ lib/Code/TidyAll/Util.pm
@@ -3,6 +3,7 @@ package Code::TidyAll::Util;
 use strict;
 use warnings;
 
+use File::Spec;
 use Path::Tiny 0.098 qw(tempdir);
 
 use Exporter qw(import);
@@ -11,13 +12,23 @@ our $VERSION = '0.64';
 
 our @EXPORT_OK = qw(tempdir_simple);
 
+use constant IS_WIN32 => $^O eq 'MSWin32';
+
 sub tempdir_simple {
     my $template = shift || 'Code-TidyAll-XXXX';
 
-    return tempdir(
-        { realpath => 1 },
+    my %args = (
         TEMPLATE => $template,
         CLEANUP  => 1
+    );
+    if (IS_WIN32) {
+        require Win32;
+        $args{DIR} = Win32::GetLongPathName( File::Spec->tmpdir );
+    }
+
+    return tempdir(
+        { realpath => 1 },
+        %args,
     );
 }
 
